e1fe03df272e22017b105a7840d5387d53d22107
Lars Atzinger <lars.atzinger@aol.de>
2021-01-18 19:26:05 +0100

    feat(): added missing endpoints in LuisAppService and added loading of App JSON from backend

--- src/app/models/Intent.ts
+++ src/app/models/Intent.ts
@@ -1,3 +1,4 @@
 export class Intent{
     name: string;
+    feature: string[];
 }
--- src/app/services/luis-app.service.ts
+++ src/app/services/luis-app.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from "@angular/common/http";
 import { environment } from '../../environments/runtime-environment';
 import { Observable } from 'rxjs';
-import {LuisApp} from '../models/LuisApp';
-import {LuisAppStats} from '../models/LuisAppStats';
-
+import { LuisApp } from '../models/LuisApp';
+import { LuisAppStats } from '../models/LuisAppStats';
+import { Intent } from '../models/Intent';
+import { Utterance } from '../models/Utterance';
+import { Entity } from '../models/Entity';
 
 @Injectable({
   providedIn: 'root'
@@ -21,23 +23,53 @@ export class LuisAppService {
     return this.httpClient.get<Array<LuisApp>>(url);
   }
 
+  public addIntent(appName: string, intent: Intent): Observable<HttpResponse<any>> {
+    const url = this.baseUrl + this.endpoint + "/addIntent";
+    return this.httpClient.post<any>(url, intent, { headers: new HttpHeaders({ "Content-Type": "application/json" }), params: { "name": appName }, observe: 'response'});
+  }
+
+  public addUtterances(appName: string, utterances: Array<Utterance>): Observable<HttpResponse<any>> {
+    const url = this.baseUrl + this.endpoint + "/addUtterances";
+    return this.httpClient.post<any>(url, utterances, { headers: new HttpHeaders({ "Content-Type": "application/json" }), params: { "name": appName }, observe: 'response'});
+  }
+
+  public addEntity(appName: string, entity: Entity): Observable<HttpResponse<any>> {
+    const url = this.baseUrl + this.endpoint + "/addEntity";
+    return this.httpClient.post<any>(url, entity, { headers: new HttpHeaders({ "Content-Type": "application/json" }), params: { "name": appName }, observe: 'response'});
+  }
+
+  public getAppJSON(appName: string): Observable<string> {
+    const url = this.baseUrl + this.endpoint + "/getJSON";
+    return this.httpClient.get<string>(url, { params: { "name": appName }});
+  }
+
+  public getHitCount(appName: string): Observable<number> {
+    const url = this.baseUrl + this.endpoint + "/getHitCount";
+    return this.httpClient.get<number>(url, { params: { "name": appName }});
+  }
+
+  public getSimpleHit(appName: string, utterance: string): Observable<string> {
+    const url = this.baseUrl + this.endpoint + "/simpleHit";
+    return this.httpClient.get<string>(url, { params: { "name": appName , utterance: utterance}});
+  }
+
   public getAppStats(appName: string): Observable<Array<LuisAppStats>> {
     const url = this.baseUrl + this.endpoint + "/getAppStats";
-    return this.httpClient.get<Array<LuisAppStats>>(url, {params: new HttpParams().set("name", appName)});
+    return this.httpClient.get<Array<LuisAppStats>>(url, { params: {"name": appName }});
   }
-  
-  public deleteApp(appName: string, force: boolean = true): Observable<any> {
+
+  public deleteApp(appName: string, force: boolean = true): Observable<HttpResponse<any>> {
     const url = this.baseUrl + this.endpoint + "/deleteApp";
-    return this.httpClient.delete(url, {params: new HttpParams().set("name", appName).set("force", String(force))});
+    return this.httpClient.delete(url, { params: new HttpParams().set("name", appName).set("force", String(force)), observe: 'response' });
   }
-  public convertCsvToJson(csv:string,name:string):Observable<string> 
-  {   
-      return this.httpClient.post<string>(this.baseUrl+"/luis/convert/convertToJSON", csv, {headers: new HttpHeaders({"Content-Type": "application/json"}), params: {"name": name}});  
+
+  public convertCsvToJson(csv: string, name: string): Observable<string> {
+    return this.httpClient.post<string>(this.baseUrl + "/luis/convert/convertToJSON", csv, { headers: new HttpHeaders({ "Content-Type": "application/json" }), params: { "name": name } });
   }
-  public convertJsonToCSV(json:string):Observable<string> 
-  {   
+
+  public convertJsonToCSV(json: string): Observable<string> {
     const headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
-      return this.httpClient.post<string>(this.baseUrl+"/luis/convert/convertToCSV", json, {headers: new HttpHeaders({"Content-Type": 'text/plain; charset=utf-8'}),responseType:'text' as 'json'});    
+    return this.httpClient.post<string>(this.baseUrl + "/luis/convert/convertToCSV", json, { headers: new HttpHeaders({ "Content-Type": 'text/plain; charset=utf-8' }), responseType: 'text' as 'json' });
   }
 
 }
