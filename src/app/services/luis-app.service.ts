8f6688cf6b445f6a02dfc91423d2dbafb2aefa68
Lars Atzinger <lars.atzinger@aol.de>
2021-01-26 22:05:39 +0100

    fix(LuisAppService): fixed post requests

--- src/app/services/luis-app.service.ts
+++ src/app/services/luis-app.service.ts
@@ -18,55 +18,55 @@ export class LuisAppService {
   constructor(private httpClient: HttpClient) { }
 
   public addIntent(appName: string, intent: Intent): Observable<HttpResponse<any>> {
-    return this.httpClient.post<any>(this.buildUrl("/addIntent"), intent, { headers: new HttpHeaders({ "Content-Type": "application/json" }), params: { "name": appName }, observe: 'response' });
+    return this.httpClient.post<any>(this.buildUrl("/addIntent"), intent, { headers: new HttpHeaders({ "Content-Type": "application/json" }), params: { name: appName }, observe: 'response' });
   }
 
   public addUtterances(appName: string, utterances: Array<Utterance>): Observable<HttpResponse<any>> {
-    return this.httpClient.post<any>(this.buildUrl("/addUtterances"), utterances, { headers: new HttpHeaders({ "Content-Type": "application/json" }), params: { "name": appName }, observe: 'response' });
+    return this.httpClient.post<any>(this.buildUrl("/addUtterances"), utterances, { headers: new HttpHeaders({ "Content-Type": "application/json" }), params: { name: appName }, observe: 'response' });
   }
 
   public addEntity(appName: string, entity: Entity): Observable<HttpResponse<any>> {
-    return this.httpClient.post<any>(this.buildUrl("/addEntity"), entity, { headers: new HttpHeaders({ "Content-Type": "application/json" }), params: { "name": appName }, observe: 'response' });
+    return this.httpClient.post<any>(this.buildUrl("/addEntity"), entity, { headers: new HttpHeaders({ "Content-Type": "application/json" }), params: { name: appName }, observe: 'response' });
   }
-  
+
   public getHitCount(appName: string): Observable<number> {
-    return this.httpClient.get<number>(this.buildUrl("/getHitCount"), { params: { "name": appName } });
+    return this.httpClient.get<number>(this.buildUrl("/getHitCount"), { params: { name: appName } });
   }
 
   public getSimpleHit(appName: string, utterance: string): Observable<string> {
-    return this.httpClient.get<string>(this.buildUrl("/simpleHit"), { params: { "name": appName, "utterance": utterance } });
+    return this.httpClient.get<string>(this.buildUrl("/simpleHit"), { params: { name: appName, utterance: utterance } });
   }
 
   public deleteApp(appName: string, force: boolean = true): Observable<HttpResponse<any>> {
-    return this.httpClient.delete(this.buildUrl("/deleteApp"), { params: new HttpParams().set("name", appName).set("force", String(force)), observe: 'response' });
+    return this.httpClient.delete(this.buildUrl("/deleteApp"),{ params: { name: appName, force: String(force)}, observe: 'response' });
   }
 
   public trainApp(appName: string): Observable<HttpResponse<any>> {
-    return this.httpClient.post<any>(this.buildUrl("/train"), { params: { "name": appName }, observe: 'response' });
+    return this.httpClient.post<any>(this.buildUrl("/train"),null, {params: {name: appName}, observe: 'response'});
   }
 
-  public createApp(json: string): Observable<string>  {
+  public createApp(json: string): Observable<string> {
     return this.httpClient.post<string>(this.buildUrl("createApp"), json, { headers: new HttpHeaders({ "Content-Type": 'text/plain; charset=utf-8' }), responseType: 'text' as 'json' });
   }
 
-  public publish(name: string, staging: boolean): Observable<HttpResponse<any>>  {
-    return this.httpClient.post<any>(this.buildUrl("/publish"), name, { headers: new HttpHeaders({ "Content-Type": 'text/plain; charset=utf-8' }), params: { "staging": String(staging) }, observe: 'response'});
+  public publish(appName: string, staging: boolean): Observable<HttpResponse<any>> {
+    return this.httpClient.post<any>(this.buildUrl("/publish"), null, { headers: new HttpHeaders({ "Content-Type": 'text/plain; charset=utf-8' }), params: { name: appName, staging: String(staging) }, observe: 'response' });
   }
 
-  public getPublishSettings(name: string): Observable<string>  {
-    return this.httpClient.post<string>(this.buildUrl("/getPublishSettings"), name, { headers: new HttpHeaders({ "Content-Type": 'text/plain; charset=utf-8' }), responseType: 'text' as 'json' });
+  public getPublishSettings(appName: string): Observable<string> {
+    return this.httpClient.get<string>(this.buildUrl("/getPublishSettings"), { headers: new HttpHeaders({ "Content-Type": 'text/plain; charset=utf-8' }), params: {name: appName}, responseType: 'text' as 'json' });
   }
 
-  public batchTestApp(appName: string, intent: string): Observable<Array<LuisAppStats>>  {
-    return this.httpClient.post<Array<LuisAppStats>>(this.buildUrl("/batchTest"), {params: {"name": appName, "intent": intent}});
+  public batchTestApp(appName: string, intent: string): Observable<Array<LuisAppStats>> {
+    return this.httpClient.post<Array<LuisAppStats>>(this.buildUrl("/batchTest"),null, { params: { name: appName, intent: intent } });
   }
 
   public getTestData(appName: string): Observable<Array<any>> {
-    return this.httpClient.get<any>(this.buildUrl("/getTestDataJSON"), { params: { "name": appName} });
+    return this.httpClient.get<any>(this.buildUrl("/getTestDataJSON"), { params: { name: appName } });
   }
 
   private buildUrl(uri: string): string {
     return this.baseUrl + this.endpoint + uri;
   }
-  
+
 }
